090d3c13b264124d7290001fb181053ca9b3ef50
KnyazkovAlexey <knyazkov_1992@mail.ru>
2023-02-05 14:16:38 +0300

    Fixed readme, removed commented code

--- app/src/Repository/ProductRepository.php
+++ app/src/Repository/ProductRepository.php
@@ -39,30 +39,11 @@ class ProductRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Product[] Returns an array of Product objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Product
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * @param int $product_id
+     * @return Product|null
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
     public function findOneByProductId(int $product_id): ?Product
     {
         return $this->createQueryBuilder('p')
